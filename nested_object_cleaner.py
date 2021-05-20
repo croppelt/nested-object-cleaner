d84dbdb3c5963be8fb6b0a000e91cde76433aa7c
Christopher Roppelt <christopher.roppelt@gmx.de>
2021-05-20 10:40:27 +0200

    Add functional path blacklisting via `ignore` parameter

--- nested_object_cleaner.py
+++ nested_object_cleaner.py
@@ -5,7 +5,7 @@ import copy
 import jsmin
 import json
 import os
-from typing import Any, Dict, Iterable, Optional, OrderedDict
+from typing import Any, Dict, Iterable, List, Optional, OrderedDict
 
 
 def get_ordered_dict_from_file(fn: str) -> OrderedDict:
@@ -51,8 +51,18 @@ def get_summed_frequencies(freq: Dict[Any, int]):
     return sum(freq.values())
 
 
-def prune_obj(obj: Any, on_keys: Iterable[Any], for_values: Iterable[Any]):
+def prune_obj(
+    obj: Any,
+    on_keys: Iterable[Any],
+    for_values: Iterable[Any],
+    ignore: Optional[Iterable[str]] = None,
+    path: Optional[List[Any]] = None,
+) -> Any:
     """Remove elements from nested `obj` where given keys match specified values."""
+    if not path:
+        path = []
+    if ignore and path and ".".join(path) in ignore:
+        return obj  # current path is 'blacklisted': won't inspect it further/deeper
     if isinstance(obj, dict):
         pruned = {}
         for k, v in obj.items():
@@ -60,16 +70,30 @@ def prune_obj(obj: Any, on_keys: Iterable[Any], for_values: Iterable[Any]):
                 return None  # current `obj` matches criteria!
             else:
                 if v:
-                    pruned_v = prune_obj(obj=v, on_keys=on_keys, for_values=for_values)
+                    path.append(k)
+                    pruned_v = prune_obj(
+                        obj=v,
+                        on_keys=on_keys,
+                        for_values=for_values,
+                        ignore=ignore,
+                        path=path,
+                    )
                     if pruned_v:
                         pruned[k] = pruned_v
+                    path.pop()
                 else:
                     pruned[k] = v  # avoids losing empty but meaningful dicts or lists
     elif isinstance(obj, list):
         pruned = []
         for element in obj:
             if element:
-                pruned_e = prune_obj(obj=element, on_keys=on_keys, for_values=for_values)
+                pruned_e = prune_obj(
+                    obj=element,
+                    on_keys=on_keys,
+                    for_values=for_values,
+                    ignore=ignore,
+                    path=path
+                )
                 if pruned_e:
                     pruned.append(pruned_e)
             else:
@@ -80,7 +104,12 @@ def prune_obj(obj: Any, on_keys: Iterable[Any], for_values: Iterable[Any]):
     return pruned
 
 
-def clean_obj(obj: Any, search_keys: Iterable[Any], clean_keys: Iterable[Any]) -> Any:
+def clean_obj(
+    obj: Any,
+    search_keys: Iterable[Any],
+    clean_keys: Iterable[Any],
+    ignore: Optional[Iterable[str]] = None,
+) -> Any:
     """Remove obsolete elements from nested `obj`."""
 
     cleaned_obj = copy.deepcopy(obj)
@@ -97,7 +126,8 @@ def clean_obj(obj: Any, search_keys: Iterable[Any], clean_keys: Iterable[Any]) -
             cleaned_obj = prune_obj(
                 obj=cleaned_obj,
                 on_keys=clean_keys,
-                for_values=prune_values
+                for_values=prune_values,
+                ignore=ignore,
             )
             freq_after = get_substr_frequency(
                 string=json.dumps(cleaned_obj),
@@ -114,14 +144,15 @@ def clean_obj(obj: Any, search_keys: Iterable[Any], clean_keys: Iterable[Any]) -
 
 
 if __name__ == "__main__":
-    target_fn: str = ""
+    target_fn: str = "nested_obj_01.json"
     nested_obj = get_ordered_dict_from_file(
         fn=target_fn,
     )
     cleaned = clean_obj(
         obj=nested_obj,
-        search_keys=["id", "source", "elementId"],
-        clean_keys=["id"],
+        search_keys=[],
+        clean_keys=[],
+        ignore=[],
     )
     write_dict_to_json(
         dict_=cleaned,
